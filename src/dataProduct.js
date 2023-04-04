689b223b27eb51b80d6fa8c2fd543ec19c020175
chattq <baotuyet927@gmail.com>
2023-04-04 10:16:07 +0700

    update

--- src/dataProduct.js
+++ src/dataProduct.js
@@ -1,15 +1,21 @@
 const productData = [
   {
     id: 1,
-    name: "e stud horse shoe oxidize",
-    codeProduct: "SV - 12341",
+    nameProduct: "e stud horse shoe oxidize",
+    codeProduct: "SV - 12341", // mã sản phẩm
     desc: "Ngoài những phong cách cá tính,phóng khoáng hoặc là nhẫn có thể đeo được ngón tay và ngón chân của KaT Jewelry ra thì có 1 kiểu nhẫn sẽ làm bạn nán lại shop vì sự độc lạ của nó.Nhẫn bạc độc lạ cao cấp 925 thiết kế - INDI RINGS được lấy cảm hứng từ đất nước Ấn độ ,với nền văn hóa đa dạng chắc chắn sẽ làm bạn thích thú.",
-    imgMain: "",
-    imgProduct: [""],
+    imgMain:
+      "https://bizweb.dktcdn.net/thumb/1024x1024/100/302/551/products/beo-5750-1.jpg?v=1586324241790",
+    imgProduct: [
+      "https://bizweb.dktcdn.net/thumb/1024x1024/100/302/551/products/beo-5750-1.jpg?v=1586324241790",
+      "https://bizweb.dktcdn.net/thumb/1024x1024/100/302/551/products/beo-5749-1.jpg?v=1586324229737",
+    ],
     price: 690000,
     priceSale: 400000,
     option: ["Vàng", "Bạc"],
-    category: "Sad",
-    sale: true,
+    category: "Sad", // phân loại sản phẩm tương tự
+    sale: true, // true là hàng đó được sale, fasle là không
+    status: true, // tình trạng đơn hàng, true là còn hàng, false là hết hàng
+    sold: 10, // số sản phẩm đã bán
   },
 ];
